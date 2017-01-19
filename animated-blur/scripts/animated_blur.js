5384745d10b2858d82f32d45c8bb626f85079726
Yi Gu <yigu@chromium.org>
2017-01-19 15:52:30 -0500

    Blur out background using SVG && foreign object

--- animated-blur/scripts/animated_blur.js
+++ animated-blur/scripts/animated_blur.js
@@ -22,7 +22,9 @@ function initializeAnimatedBlur(element) {
   var animatedBlur = false;
   var inOrOut = 0;
   var num = 4;
-  var blurURL = "";
+  var currentTooltip;
+  var currentImg;
+  var currentBlurEvent;
 
   function addKeyFrames(name, id) {
     var keyframes = '@keyframes ' + name + ' {';
@@ -41,29 +43,47 @@ function initializeAnimatedBlur(element) {
     }
   }
 
-  function blurInOrOut(e, inOrOut) {
+  // inOrOut:
+  //   1 : blur out
+  //  -1 : blur in
+  //   0 : do nothing
+  function startBlur(e, inOrOut) {
     if (!inOrOut) return;
-
     for (var i = 0; i < num; ++i) {
       var svg = inOrOut > 0 ? document.body.querySelector('#b' + (i + 1))
           : document.body.querySelector('#b' + (num - i));
       svg.style.animation = 'b' + (i + 1) + '-anim 2s forwards linear';
     }
+    // Tooltip and temporary img blur out
+    if (inOrOut == -1) {
+      if (currentImg) {
+        currentImg.style.animation = 'b1-anim 2s forwards linear';
+      }
+      if (currentTooltip) {
+        currentTooltip.style.animation = 'b1-anim 2s forwards linear';
+      }
+      document.getElementById('animated-blur').style.animation =
+          'b4-anim 2s forwards linear';
+    } else {
+      document.getElementById('animated-blur').style.animation =
+          'b1-anim 2s forwards linear';
+    }
   }
 
   function displayToolTips(e, inOrOut) {
-    if (inOrOut == -1 ) {
-      document.querySelector('#toolTip').remove();
-      return;
-    }
+    if (!inOrOut) return;
+    if (e.target.nextElementSibling.localName != 'figcaption') return;
     var width = element.clientWidth;
     var height = element.clientHeight;
 
     var svg = document.createElementNS(svgns, 'svg');
     svg.id = 'toolTip';
+    svg.style.top = element.offsetTop + 'px';
+    svg.style.left = element.offsetLeft + 'px';
     svg.setAttribute('width', width);
     svg.setAttribute('height', height);
     document.body.appendChild(svg);
+    currentTooltip = svg;
 
     var g = document.createElementNS(svgns, 'g');
     svg.appendChild(g);
@@ -72,8 +92,10 @@ function initializeAnimatedBlur(element) {
     var t = 50, k = 15;
     var tip = {'w': (3/4 * t), 'h': k};
     var fo = document.createElementNS(svgns, 'foreignObject');
-    fo.setAttribute('x', anchor.w - tip.w);
-    fo.setAttribute('y', anchor.h + tip.h);
+    var tooltipX = anchor.w - tip.w + foWidth > element.clientWidth ?
+        element.clientWidth - foWidth - tip.w : anchor.w - tip.w;
+
+    fo.setAttribute('x', tooltipX);
     fo.setAttribute('width', foWidth);
     fo.setAttribute('class', 'svg-tooltip');
     g.appendChild(fo);
@@ -82,70 +104,46 @@ function initializeAnimatedBlur(element) {
     fo.appendChild(div);
     var p = document.createElementNS(xhtmlns, 'p');
     p.setAttribute('class', 'lead');
-    p.innerHTML = e.currentTarget.querySelector('img').alt;
+    p.innerHTML = e.target.alt;
     div.appendChild(p);
     p = document.createElementNS(xhtmlns, 'p');
-    p.innerHTML = e.currentTarget.querySelector('#desc').innerText;
+
+    p.innerHTML = e.target.nextElementSibling.innerText;
     div.appendChild(p);
     var foHeight = div.getBoundingClientRect().height;
     fo.setAttribute('height', foHeight);
+    var tooltipY = anchor.h + tip.h + foHeight > element.clientHeight ?
+    element.clientHeight - foHeight - tip.h : anchor.h + tip.h;
+    fo.setAttribute('y', tooltipY);
     var polygon = document.createElementNS(svgns, 'polygon');
-    polygon.setAttribute('points', "0,0 0," + foHeight + " " + foWidth + "," +
-        foHeight + " " + foWidth + ",0 " + (t) + ",0 " + tip.w + "," + (-tip.h)
-        + " " + (t/2) + ",0");
+    polygon.setAttribute('points', "0,0 0," + foHeight + " " + foWidth +
+        "," + foHeight + " " + foWidth + ",0 " + (t) + ",0 " + tip.w +
+        "," + (-tip.h) + " " + (t/2) + ",0");
     polygon.setAttribute('height', foHeight + tip.h);
     polygon.style.height = foHeight + tip.h;
     polygon.setAttribute('width', foWidth);
     polygon.setAttribute('fill', '#D8D8D8');
     polygon.setAttribute('opacity', 0.75);
-    polygon.setAttribute('transform', 'translate(' + (anchor.w - tip.w) + ',' +
-          (anchor.h + tip.h) + ')');
+    polygon.setAttribute('transform', 'translate(' + tooltipX + ',' +
+          tooltipY + ')');
     g.insertBefore(polygon, fo);
   }
 
-  function createObjects(e, num) {
-    var img = e.currentTarget.querySelector('img');
-    for (var i = 0; i < num; ++i) {
-      var svg = document.createElementNS(svgns, 'svg');
-      svg.id = 'b' + (i + 1);
-      svg.setAttribute('width', img.width);
-      svg.setAttribute('height', img.height);
-      e.currentTarget.appendChild(svg);
-      var defs = document.createElementNS(svgns, 'defs');
-      svg.appendChild(defs);
-      var filter = document.createElementNS(svgns, 'filter');
-      filter.id = 'f' + (i + 1);
-      filter.setAttribute('x', 0);
-      filter.setAttribute('y', 0);
-      defs.append(filter)
-      var feGaussianBlur = document.createElementNS(svgns, 'feGaussianBlur');
-      feGaussianBlur.setAttribute('in', 'SourceGraphic');
-      feGaussianBlur.setAttribute('stdDeviation', 4 * i);
-      filter.append(feGaussianBlur);
-      var image = document.createElementNS(svgns, 'image');
-      image.id = 'img';
-      image.setAttribute('href', img.currentSrc);
-      image.setAttribute('x', 0);
-      image.setAttribute('y', 0);
-      image.setAttribute('width', img.width);
-      image.setAttribute('height', img.height);
-      image.setAttribute('filter', 'url(#f' + (i + 1) + ')');
-      svg.append(image);
-      addKeyFrames('b' + (i + 1) + '-anim', i);
-    }
-  }
-
-  function cloneElement(num) {
+  function buildSVGs(num) {
+    var width = element.clientWidth;
+    var height = element.clientHeight;
     for (var i = 1; i <= num; ++i) {
       var svg = document.createElementNS(svgns, 'svg');
       svg.id = 'b' + i;
-      svg.setAttribute('width', element.clientWidth);
-      svg.setAttribute('height', element.clientHeight);
+      svg.style.top = element.offsetTop + 'px';
+      svg.style.left = element.offsetLeft + 'px';
+      svg.setAttribute('width', width);
+      svg.setAttribute('height', height);
 
       var filter = document.createElementNS(svgns, 'filter');
       filter.id = 'f' + i;
-      filter.setAttribute('width', '100%');
-      filter.setAttribute('height', '100%');
+      filter.setAttribute('width', width);
+      filter.setAttribute('height', height);
       svg.append(filter);
       var feGaussianBlur = document.createElementNS(svgns, 'feGaussianBlur');
       feGaussianBlur.setAttribute('stdDeviation', 4 * (i - 1));
@@ -153,13 +151,12 @@ function initializeAnimatedBlur(element) {
 
       var fo = document.createElementNS(svgns, 'foreignObject');
       fo.setAttribute('filter', 'url(#f' + i + ')');
-      fo.setAttribute('width', '100%');
-      fo.setAttribute('height', '100%');
+      fo.setAttribute('width', width);
+      fo.setAttribute('height', height);
 
       var container = document.createElementNS(xhtmlns, 'div');
       container.innerHTML = element.innerHTML;
       fo.appendChild(container);
-
       svg.appendChild(fo);
 
       document.body.appendChild(svg);
@@ -167,18 +164,69 @@ function initializeAnimatedBlur(element) {
     }
   }
 
+  function reset() {
+    var svgs = document.body.querySelectorAll('svg[id^="b"]');
+    for (var i = 0; i < svgs.length; ++i) {
+      svgs[i].remove();
+    }
+    if (currentTooltip) {
+      currentTooltip.remove();
+      currentTooltip = null;
+    }
+  }
+
+  function displayCurrentImg(e) {
+    currentImg = e.target.cloneNode(true);
+    currentImg.setAttribute('class', 'selected');
+    currentImg.style.left = e.target.x + 'px';
+    currentImg.style.top = e.target.y + 'px';
+    document.body.appendChild(currentImg);
+  }
+
+  function reset() {
+    if (currentTooltip) {
+      currentTooltip.remove();
+      currentTooltip = null;
+    }
+    var svgs = document.body.querySelectorAll('svg[id^="b"]');
+    for (var i = 0; i < svgs.length; ++i) {
+      svgs[i].remove();
+    }
+    if (currentImg) {
+      currentImg.remove();
+      currentImg = null;
+    }
+  }
+
+  function prepareToBlur() {
+    buildSVGs(num);
+    displayCurrentImg(currentBlurEvent);
+    displayToolTips(currentBlurEvent, inOrOut);
+  }
+
+  window.onresize = function() {
+    reset();
+    if (!currentBlurEvent) return;
+    prepareToBlur();
+  }
+
   document.onclick = function(e) {
     if (!animatedBlur) {
-      cloneElement(num);
-      inOrOut = 1;
-      animatedBlur = true;
+      reset();
+      if (e.target.localName == 'img') {
+        inOrOut = 1;
+        animatedBlur = true;
+        currentBlurEvent = e;
+        prepareToBlur();
+      } else {
+        inOrOut = 0;
+      }
     } else {
       inOrOut *= -1;
+      animatedBlur = false;
+      currentBlurEvent = null;
     }
-    blurInOrOut(e, inOrOut);
-    displayToolTips(e, inOrOut);
+    startBlur(e, inOrOut);
   }
 
 }
-
-
